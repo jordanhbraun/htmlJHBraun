6d0373259948f91d276eef49a8e6a6d93decade3
jordanhbraun <jordan@greatjob.is>
2014-06-07 22:44:53 -0500

    twitter api integration

--- get-tweets1.1.php
+++ get-tweets1.1.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-require_once('twitteroauth.php');
+require_once("twitteroauth/twitteroauth/twitteroauth.php"); //Path to twitteroauth library
  
 $twitteruser = "DESIGNJORDAN";
-$notweets = 10;
+$notweets = 30;
 $consumerkey = "Hjc4hU1RTr9yeau0s4OrXbHFV";
 $consumersecret = "6xfPcRE1jv7jlO8sgXWguu2OdoVMk4mmndKBhKPpoYazcOe0ur";
 $accesstoken = "2484115562-s7fn9nPaBFlcrn8irSiyp0LOyvWrVLv1CiYJHVD";
@@ -13,7 +13,7 @@ function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oa
   $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
   return $connection;
 }
-  
+ 
 $connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
  
 $tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
